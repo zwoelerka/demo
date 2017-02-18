028499539b570a1040d2b9a6352e5b6de4ff9a51
Maja <maja.todorovska@avicenna4it.com>
2017-02-18 15:59:02 +0100

    content in blocks

--- js/app.js
+++ js/app.js
@@ -1,24 +1,34 @@
 var demoApp = angular.module('demoApp', []);
 
-demoApp.controller('demoController', function demoController($scope) {
+demoApp.controller('demoController', function demoController($scope, $timeout) {
     $scope.callBtnVisible = 1;
-
-
+   
 	$scope.hideCallBtn = function () {
 		$scope.callBtnVisible = 0;
 	}
 	$scope.showCallBtn = function () {
 		$scope.callBtnVisible = 1;
 	}
+
+	$timeout(function () {
+         	$scope.navbarHeight = document.getElementById("navbar").offsetHeight;
+		    $scope.navbarHeight = $scope.navbarHeight.toString() + 'px';
+		    $scope.mainHeight = 'calc(100vh - ' + $scope.navbarHeight + ')';
+
+			$scope.mainStyle = {
+				"color" : "red",
+		        "min-height" : $scope.mainHeight
+		    }
+          });
+
 });
 
 
 function startCall() {
-    var div1 = document.getElementById("remoteImg");
-    div1.style.width = "0";
+   
     var div2 = document.getElementById("remotesVideos");
-    div2.style.width = "98%";
-    div2.style.height = "98%";
+    div2.style.width = "100%";
+    div2.style.height = "101.5%";
 
 
     var div3 = document.getElementById("localImg");
